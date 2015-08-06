2311d21343281ee80bfba0710a7b871d6bc37874
Meng-Gen Tsai <menggen@thermalmix-lm.tpcity.corp.yahoo.com>
2015-08-06 18:08:57 +0800

    Add balance sheet spider

---
+++ stockcat/spider/balance_sheet_spider.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+
+from stockcat.spider.ifrs.ifrs_balance_sheet_spider import IfrsBalanceSheetSpider
+from stockcat.spider.legacy.legacy_balance_sheet_spider import LegacyBalanceSheetSpider
+
+import datetime
+
+class BalanceSheetSpider():
+    def __init__(self):
+        self.ifrs_spider = IfrsBalanceSheetSpider()
+        self.legacy_spider = LegacyBalanceSheetSpider()
+
+    def crawl(self, stock_symbol, date):
+        # IFRS are available from 2013 to now
+        if date >= datetime.date(2013, 1, 1):
+            return self.ifrs_spider.crawl(stock_symbol, date)
+        # Otherwise we use legacy data
+        else:
+            return self.legacy_spider.crawl(stock_symbol, date)
+
+    def is_crawled(self, stock_symbol, date):
+        # IFRS are available from 2013 to now
+        if date >= datetime.date(2013, 1, 1):
+            return self.ifrs_spider.is_crawled(stock_symbol, date)
+        # Otherwise we use legacy data
+        else:
+            return self.legacy_spider.is_crawled(stock_symbol, date)
